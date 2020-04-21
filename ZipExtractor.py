2ae6240db9feaada0775588f3422c4ebebfca425
B.Praveen Kumar <31960965+PRAVEEN11KUMAR@users.noreply.github.com>
2020-04-21 19:26:04 +0530

    Update ZipExtractor.py

--- ZipExtractor.py
+++ ZipExtractor.py
@@ -1,6 +1,17 @@
+# zipextractor.py
+
 from zipfile import ZipFile
 
 filepath = "F:\python/praveen.zip"
 with ZipFile(filepath, "r") as zip:
     zip.printdir()
     zip.extractall()
+
+    
+#  zipfile.py
+
+import zipfile
+
+zf = zipfile.ZipFile("praveen.zip", "w")
+zf.write("praveen.txt", compress_type=zipfile.ZIP_DEFLATED)
+zf.close()
